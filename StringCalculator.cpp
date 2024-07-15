872b725bb957ace6a5d11b2fc1ac7a93c0f05dc2
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-07-15 16:06:04 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,34 +1,28 @@
-#include <iostream>
-#include <string>
+#include "StringCalculator.h"
 #include <sstream>
-#include <vector>
 
-class StringCalculator {
-public:
-    int add(const std::string& numbers) {
-        if (numbers.empty()) {
-            return 0;
-        }
-        
-        std::vector<int> numList = splitAndConvert(numbers);
-        int sum = 0;
-        for (int num : numList) {
-            sum += num;
-        }
-        
-        return sum;
+int StringCalculator::add(const std::string& numbers) {
+    if (numbers.empty()) {
+        return 0;
     }
+    
+    std::vector<int> numList = splitAndConvert(numbers);
+    int sum = 0;
+    for (int num : numList) {
+        sum += num;
+    }
+    
+    return sum;
+}
 
-private:
-    std::vector<int> splitAndConvert(const std::string& numbers) {
-        std::vector<int> result;
-        std::stringstream ss(numbers);
-        std::string item;
-        
-        while (std::getline(ss, item, ',')) {
-            result.push_back(std::stoi(item));
-        }
-        
-        return result;
+std::vector<int> StringCalculator::splitAndConvert(const std::string& numbers) {
+    std::vector<int> result;
+    std::stringstream ss(numbers);
+    std::string item;
+    
+    while (std::getline(ss, item, ',')) {
+        result.push_back(std::stoi(item));
     }
-};
+    
+    return result;
+}
